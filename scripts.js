8e9fe129f5e47a8110feef53f5d2867b92f04600
Eryck Assis <eng.assis.dev@gmail.com>
2025-07-22 20:09:33 -0300

    Refactor image upload functionality and improve file handling

--- scripts.js
+++ scripts.js
@@ -1,36 +1,38 @@
 const uploadBtn = document.getElementById("upload-btn");
-const inputUpload = document.getElementById("image-upload");
-
-document
-  .getElementById("imageInput")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        const previewImage = document.getElementById("previewImage");
-        previewImage.src = e.target.result;
-        previewImage.style.display = "block";
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+const inputUpload = document.getElementById("image-upload")
 
 uploadBtn.addEventListener("click", () => {
-  inputUpload.click();
-});
+    inputUpload.click();
+})
 
-function lerConteudo(arquivo) {
-  return new Promise((resolve, reject) => {
-    const leitor = new FileReader();
-    leitor.onload = () => {
-      resolve({ url: leitor.result, nome: arquivo.name });
-    };
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name })
+        }
 
-    leitor.onerror = () => {
-      reject(`Erro na leitura do arquivo: ${arquivo.name}`);
-    };
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
+        }
 
-    leitor.readAsDataURL(arquivo);
-  });
+        leitor.readAsDataURL(arquivo)
+    })
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch (erro) {
+            console.error("Erro na leitura do arquivo")
+        }
+    }
+})
